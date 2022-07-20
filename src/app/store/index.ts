8e3045e244b329a7ab0fbdb46562280ca679d663
Suraj Bhangale <Suraj.Bhangale@GEMINI.NCIRCLETECH.COM>
2022-07-20 11:01:22 +0530

    add states and reducers to index file

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,13 +1,20 @@
 import { ActionReducerMap, MetaReducer } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import { assetDataReducer, AssetDataState, assetReducer, AssetState, measurementReducer, MeasurementState } from '../state/asset-chart.reducers';
+
 
 
 export interface AppState {
+    assetState : AssetState;
+    measurementState: MeasurementState;
+    assetDataState: AssetDataState;
 
 }
 
 export const reducers: ActionReducerMap<AppState> = {
-
+    assetState: assetReducer,
+    measurementState: measurementReducer,
+    assetDataState: assetDataReducer
 };
 
 export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
