e5fad2c714e097fb140a2d4c7997486ac76bd85f
Suraj Bhangale <Suraj.Bhangale@GEMINI.NCIRCLETECH.COM>
2022-07-24 23:04:27 +0530

    add selectors for asset and measurements

--- src/app/state/asset-chart.selectors.ts
+++ src/app/state/asset-chart.selectors.ts
@@ -2,21 +2,32 @@ import { createFeatureSelector, createSelector } from "@ngrx/store";
 import { AssetDataState, AssetState, MeasurementState, selectAll } from "./asset-chart.reducers";
 
 
+
 export const assetFeatureSelector = createFeatureSelector<AssetState>('asset');
 export const measurementFeatureSelector = createFeatureSelector<MeasurementState>('measurement');
 export const assetDataFeatureSelector = createFeatureSelector<AssetDataState>('assetData');
 
+export const getAssets = createSelector(
+  assetFeatureSelector,
+  selectAll
+);
+
 export const areAssetsLoaded = createSelector(
-    assetFeatureSelector,
+  assetFeatureSelector,
   state => state.assetsLoaded
 );
 
+export const getMeasurements = createSelector(
+  measurementFeatureSelector,
+  state => state
+);
+
 export const areMeasurementsLoaded = createSelector(
-    measurementFeatureSelector,
+  measurementFeatureSelector,
   state => state.measurementsLoaded
 );
 
 export const areAssetDataLoaded = createSelector(
     assetDataFeatureSelector,
-  state => state.setChartData
+    state => state
 );
